29472ef955c028b454ac916f45ab235c5daaaba5
Dowon Koo <cozarokk@gmail.com>
2020-12-27 11:01:10 +0900

    feat: implement basic features of board

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -2,14 +2,19 @@ import React from 'react';
 import './TodoList.scss';
 import TodoListItem from './TodoListItem';
 
-const TodoList = () => {
+const TodoList = ({ todos, onRemove, onToggle }) => {
   return (
     <div className="TodoList">
-      <TodoListItem />
-      <TodoListItem />
-      <TodoListItem />
+      {todos.map((todo) => (
+        <TodoListItem
+          todo={todo}
+          key={todo.id}
+          onRemove={onRemove}
+          onToggle={onToggle}
+        />
+      ))}
     </div>
-  )
-}
+  );
+};
 
 export default TodoList;
